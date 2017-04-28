17ae590ce0a4cc6e417d0701ca382195d7e109f5
John Bresnahan <john@stardog.com>
2017-04-28 11:10:32 -1000

    Debugging pipeline

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -u
 
 TAG=$1
 GRAV_REPO=$2
@@ -16,18 +16,22 @@ DESTNAME=$(basename $LINUX_GRAV_EXE)
 VER=$(echo $DESTNAME | sed 's/stardog-graviton-//')
 echo "TAG: $TAG"
 echo "VER: $VER"
-~/LINUX_GRAV_EXE --version  2>&1 | grep $VER
+$LINUX_GRAV_EXE --version
+$LINUX_GRAV_EXE --version  2>&1 | grep $VER
 if [ $? -ne 0 ]; then
     echo "The version information is not correct in the artifact"
     exit 1
 fi
 
+set -e
 if [ "X$TAG" != "X0" ]; then
-    ~/LINUX_GRAV_EXE --version  2>&1 | grep $TAG
+    set +e
+    $LINUX_GRAV_EXE --version  2>&1 | grep $TAG
     if [ $? -ne 0 ]; then
         echo "The version information is not correct in the artifact and tag"
         exit 1
     fi
+    set -e
     DESTNAME=stardog-graviton-$TAG
     pushd $GRAV_REPO
 
@@ -48,8 +52,12 @@ echo "make the dirs"
 ls $EXE_OUTPUT
 mkdir -p $EXE_OUTPUT/$VER
 
+echo "Create the linux zip"
 cp $LINUX_GRAV_EXE stardog-graviton
+chmod 755 stardog-graviton
 zip "$EXE_OUTPUT/$VER/stardog-graviton_$VER""_linux_amd64.zip" stardog-graviton
+echo "Create the darwin zip"
 cp $DARWIN_GRAV_EXE stardog-graviton
+chmod 755 stardog-graviton
 zip "$EXE_OUTPUT/$VER/stardog-graviton_$VER""_darwin_amd64.zip" stardog-graviton
-
+ls $EXE_OUTPUT/$VER
